16164ef57653788bee67642308ccf0711c69df8d
Arin <arinkumarjoshi@gmail.com>
2025-07-12 16:49:01 +0530

    Updated the backend/node/chat-service

---
+++ backend/node/chat-service/index.js
@@ -0,0 +1,11 @@
+import express from "express"
+import dotenv from "dotenv"
+dotenv.config();
+import { connectDB } from "./src/libs/db.js";
+const app = express();
+const port = 3000;
+app.use(express.json())
+app.listen(port, () => {
+    console.log("server is on port: " + port);
+    connectDB();
+})
