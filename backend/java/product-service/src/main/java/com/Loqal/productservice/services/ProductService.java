3cd8954e278a49c427c634d490fe13fedca808eb
Ayushman Sahoo <ayushmansahoo176@gmail.com>
2025-07-21 02:58:38 +0530

    controller class updated

--- backend/java/product-service/src/main/java/com/Loqal/productservice/services/ProductService.java
+++ backend/java/product-service/src/main/java/com/Loqal/productservice/services/ProductService.java
@@ -1,8 +1,10 @@
 package com.Loqal.productservice.services;
 
 import com.Loqal.productservice.entity.Product;
+import com.Loqal.productservice.entity.ProductDTO;
 import com.Loqal.productservice.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -12,30 +14,46 @@ import java.util.UUID;
 
 @Service
 public class ProductService {
-    @Autowired private ProductRepository repo;
+    @Autowired
+    private ProductRepository repo;
 
-    public Product create(Product p) {
-        p.setCreated_at(new Date());
-        p.setUpdated_at(new Date());
-        return repo.save(p);
+    public ResponseEntity<Product> create(ProductDTO p, UUID merchantID, Date created, Date updated) {
+
+        Product pd = new Product();
+        pd.setName(p.name());
+        pd.setDescription(p.description());
+        pd.setCategory(p.category());
+        pd.setPrice(p.price());
+        pd.setMerchantId(merchantID);
+        pd.setQuantity(p.quantity());
+        pd.setImage_urls(p.image_urls());
+        pd.setCreated_at(created);
+        pd.setUpdated_at(updated);
+
+        return ResponseEntity.ok(repo.save(pd));
+
+        ///
     }
 
-    public List<Product> getAll() {
-        return repo.findAll();
+
+
+    public ResponseEntity<List<Product>> getAll(UUID tenenant_id) {
+        return ResponseEntity.ok(repo.findAll());
     }
 
     public Optional<Product> getById(UUID id) {
+
         return repo.findById(id);
     }
 
-    public Product update(UUID id, Product updated) {
+    public Product update(UUID id, ProductDTO updated) {
         return repo.findById(id).map(existing -> {
-            existing.setName(updated.getName());
-            existing.setDescription(updated.getDescription());
-            existing.setCategory(updated.getCategory());
-            existing.setPrice(updated.getPrice());
-            existing.set_available(updated.is_available());
-            existing.setImage_urls(updated.getImage_urls());
+            existing.setName(updated.name());
+            existing.setDescription(updated.description());
+            existing.setCategory(updated.category());
+            existing.setPrice(updated.price());
+            existing.setQuantity(updated.quantity());
+            existing.setImage_urls(updated.image_urls());
             existing.setUpdated_at(new Date());
             return repo.save(existing);
         }).orElse(null);
