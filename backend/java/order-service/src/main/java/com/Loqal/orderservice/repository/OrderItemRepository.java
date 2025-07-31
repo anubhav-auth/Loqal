7f6606836ca0a99b86f4048085c4ec7ff4445f8e
anubhav-auth <anubhavjaiswal2002@gmail.com>
2025-07-31 19:05:49 +0530

    refactor: remove unused MerchantController, UserController, and repository interfaces

--- backend/java/order-service/src/main/java/com/Loqal/orderservice/repository/OrderItemRepository.java
+++
@@ -1,8 +0,0 @@
-package com.Loqal.orderservice.repository;
-
-import com.Loqal.orderservice.entity.OrderItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface OrderItemRepository extends JpaRepository<OrderItem, UUID> {}
