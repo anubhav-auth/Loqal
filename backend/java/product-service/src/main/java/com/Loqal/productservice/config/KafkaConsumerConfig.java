421c67c00a4125cc13228058b40bb5a9d371dafe
anubhav-auth <anubhavjaiswal2002@gmail.com>
2025-08-04 02:03:13 +0530

    feat: update Kafka consumer configuration to use dynamic bootstrap servers from application properties

--- backend/java/product-service/src/main/java/com/Loqal/productservice/config/KafkaConsumerConfig.java
+++ backend/java/product-service/src/main/java/com/Loqal/productservice/config/KafkaConsumerConfig.java
@@ -3,6 +3,7 @@ package com.Loqal.productservice.config;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
@@ -19,11 +20,13 @@ import java.util.Map;
 
 @Configuration
 public class KafkaConsumerConfig {
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
 
     @Bean
     public ConsumerFactory<String, Object> consumerFactory() {
         Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         configProps.put(ConsumerConfig.GROUP_ID_CONFIG, "product-service-group");
         configProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         configProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
@@ -41,9 +44,7 @@ public class KafkaConsumerConfig {
 
     @Bean
     public DefaultErrorHandler errorHandler(DeadLetterPublishingRecoverer deadLetterPublishingRecoverer) {
-        // Retry 2 times with a 1-second delay.
         FixedBackOff backOff = new FixedBackOff(1000L, 2);
-        // After retries are exhausted, pass the message to the recoverer.
         return new DefaultErrorHandler(deadLetterPublishingRecoverer, backOff);
     }
 
@@ -51,7 +52,7 @@ public class KafkaConsumerConfig {
     @Bean
     public ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory(
             ConsumerFactory<String, Object> consumerFactory,
-            DefaultErrorHandler errorHandler) { // Spring injects the errorHandler bean we just defined.
+            DefaultErrorHandler errorHandler) {
 
         ConcurrentKafkaListenerContainerFactory<String, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory);
