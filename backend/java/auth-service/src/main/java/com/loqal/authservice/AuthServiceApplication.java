e36c6b79a8d43a49a746040cc985811e75c6062f
anubhav-auth <anubhavjaiswal2002@gmail.com>
2025-08-07 03:58:09 +0530

    refactor: remove JPA repository configuration and enable R2DBC repositories in AuthServiceApplication

--- backend/java/auth-service/src/main/java/com/loqal/authservice/AuthServiceApplication.java
+++ backend/java/auth-service/src/main/java/com/loqal/authservice/AuthServiceApplication.java
@@ -5,8 +5,7 @@ import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 
 @OpenAPIDefinition(
         info = @Info(
@@ -16,8 +15,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
                 contact = @Contact(name = "Anubhav", email = "anubhavauth@gmail.com")
         )
 )
-@EntityScan("com.loqal.authservice")
-@EnableJpaRepositories("com.loqal.authservice")
+@EnableR2dbcRepositories("com.loqal.authservice.repository")
 @SpringBootApplication
 public class AuthServiceApplication {
 
