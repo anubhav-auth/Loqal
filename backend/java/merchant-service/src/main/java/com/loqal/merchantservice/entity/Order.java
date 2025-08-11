22c14cd2dbf3b2f5d26697fa9ec766c40a1fb24e
anubhav-auth <anubhavjaiswal2002@gmail.com>
2025-08-12 02:25:07 +0530

    feat: add Order and OrderItem entities with relevant fields and annotations

---
+++ backend/java/merchant-service/src/main/java/com/loqal/merchantservice/entity/Order.java
@@ -0,0 +1,35 @@
+package com.Loqal.orderservice.entity;
+
+import com.Loqal.orderservice.dto.OrderStatus;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
+import org.springframework.data.annotation.Version;
+import org.springframework.data.relational.core.mapping.Table;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Table("orders")
+@Data
+public class Order {
+    @Id
+    private UUID id;
+    private UUID customerId;
+    private UUID merchantId;
+    private UUID deliveryAgentId;
+    @Transient
+    private List<OrderItem> items;
+    private double totalAmount;
+    private double discountAmount;
+    private double finalAmount;
+    private String paymentStatus;
+    private UUID deliveryAddressId;
+    private OrderStatus currentStatus;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private UUID razorpayOrderId;
+    private UUID razorpayPaymentId;
+    @Version
+    private Long version;
+}
