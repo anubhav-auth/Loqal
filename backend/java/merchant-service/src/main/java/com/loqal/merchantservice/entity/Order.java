306cc575f82dbc949e769b88baae499498ff9ed3
anubhav-auth <anubhavjaiswal2002@gmail.com>
2025-08-12 02:25:21 +0530

    fix: correct package declarations in OrderItem and Order classes

--- backend/java/merchant-service/src/main/java/com/loqal/merchantservice/entity/Order.java
+++ backend/java/merchant-service/src/main/java/com/loqal/merchantservice/entity/Order.java
@@ -1,16 +1,16 @@
-package com.Loqal.orderservice.entity;
+package com.loqal.merchantservice.entity;
 
-import com.Loqal.orderservice.dto.OrderStatus;
+import com.loqal.merchantservice.dto.OrderItem;
+import com.loqal.merchantservice.dto.OrderStatus;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.Transient;
 import org.springframework.data.annotation.Version;
-import org.springframework.data.relational.core.mapping.Table;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
-@Table("orders")
+
 @Data
 public class Order {
     @Id
